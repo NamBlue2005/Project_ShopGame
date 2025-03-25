9d7c7140dad2023ed3bf5641f68f4fe476dfb8f5
taobitk <8hungvip8@gmail.com>
2025-03-25 16:19:28 +0700

    loại bỏ severlet

--- WebGame/src/main/java/controller/UseHome.java
+++ WebGame/src/main/java/controller/UseHome.java
@@ -7,10 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "name",urlPatterns = "/home")
-public class UserController extends HttpServlet {
+@WebServlet(name = "home",urlPatterns = "/")
+public class UseHome extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/views/home.jsp").forward(req, resp);
+        req.getRequestDispatcher("/views/user/home.jsp").forward(req, resp);
     }
 }
---
+++ WebGame/src/main/java/controller/UseLogin.java
@@ -0,0 +1,5 @@
+package controller;
+// Là khách hàng: tôi có thể đăng nhập, đăng ký tài khoản
+// đăng ký e có thể dùng modal hoặc tạo jsp mới
+public class UseLogin {
+}
